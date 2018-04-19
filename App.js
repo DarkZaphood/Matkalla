b706da2732439c414e46e870482fb80e2f90829b
DarkZaphood <37557710+DarkZaphood@users.noreply.github.com>
2018-04-19 21:18:16 +0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,19 +1,94 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-import { Logo } from './logo.js';
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
-export default class App extends React.Component {
+import CameraRollPicker from 'react-native-camera-roll-picker';
+
+import Button from 'react-native-button';
+
+export default class Example extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      num: 0,
+      selected: [],
+    };
+  }
+
+  getSelectedImages(images, current) {
+    var num = images.length;
+
+    this.setState({
+      num: num,
+      selected: images,
+    });
+
+    console.log(current);
+    console.log(this.state.selected);
+  }
+
   render() {
-    return <Logo />
+    return (
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.text}>
+            <Text style={styles.bold}> {this.state.num} </Text> images has been selected
+          </Text>
+        </View>
+
+        <CameraRollPicker
+          scrollRenderAheadDistance={500}
+          initialListSize={1}
+          pageSize={3}
+          removeClippedSubviews={false}
+          groupTypes='SavedPhotos'
+          batchSize={5}
+          maximum={5}
+          selected={this.state.selected}
+          assetType='Photos'
+          imagesPerRow={3}
+          imageMargin={5}
+          callback={this.getSelectedImages.bind(this)} />
+      </View>
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F6AE2D',
+  },
+  content: {
+    marginTop: 15,
+    height: 50,
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
     alignItems: 'center',
-    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  text: {
+    fontSize: 16,
+    alignItems: 'center',
+    color: '#fff',
+  },
+  bold: {
+    fontWeight: 'bold',
+  },
+  info: {
+    fontSize: 12,
   },
 });
+
+// AppRegistry.registerComponent('Example', () => Example);
